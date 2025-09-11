cdae22be75f5c2a8c9e8b59f651da8691b377ea5
Prashant Singh <pramar97singh@gmail.com>
2025-09-11 09:56:58 +0530

    add the initial data

--- src/index.server.js
+++ src/index.server.js
@@ -10,6 +10,7 @@ const adminRoutes = require("./routes/admin/auth");
 const categoryRoutes = require("./routes/category");
 const productRoutes = require("./routes/product");
 const cartRoutes = require("./routes/cart");
+const initialDataRoutes = require("./routes/admin/initialData");
 const cors = require("cors");
 
 env.config();
@@ -35,6 +36,7 @@ app.use("/api", adminRoutes);
 app.use("/api", categoryRoutes);
 app.use("/api", productRoutes);
 app.use("/api", cartRoutes);
+app.use("/api", initialDataRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log("Server");
