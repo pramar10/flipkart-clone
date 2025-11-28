b34d206e78f6ab102ab8233883fc7ba65606e59c
Prashant Singh <pramar97singh@gmail.com>
2025-11-28 13:35:41 +0530

    add the category api

--- src/controller/category.js
+++ src/controller/category.js
@@ -101,7 +101,7 @@ exports.deleteCategories = async (req, res) => {
   for (let i = 0; i < ids.length; i++) {
     const deleteCategory = await Category.findOneAndDelete({
       _id: ids[i]._id,
-      createdBy: req.user._id,
+      // createdBy: req.user._id,
     });
     deletedCategories.push(deleteCategory);
   }
