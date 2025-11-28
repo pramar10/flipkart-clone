7c92ab8a6d5fe96087bff4b9c2f54576e16b315c
Prashant Singh <pramar97singh@gmail.com>
2025-11-28 19:07:01 +0530

    update the react-redux

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,7 @@
 import { applyMiddleware, createStore } from "redux";
 import rootReducer from "../reducers";
-import { thunk } from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
-
+import thunk from "redux-thunk";
 const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(thunk))
